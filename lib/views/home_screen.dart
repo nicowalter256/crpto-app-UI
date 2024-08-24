a1e46592183cec7d19fa8fbf9af83890e30bd675
Nicopee <gidudunicholas11@gmail.com>
2024-08-24 13:24:25 +0300

    removed stuff

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -129,10 +129,6 @@ class _HomeScreenState extends State<HomeScreen> {
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [Text("14750"), Text("+2.56%")],
                     ),
-                    const Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [Text("14750"), Text("+2.56%")],
-                    )
                   ],
                 ),
               )
