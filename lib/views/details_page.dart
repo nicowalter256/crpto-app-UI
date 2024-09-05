1017f5daa5976ed9d4fc3037c2a2c200956db0b8
Nicopee <gidudunicholas11@gmail.com>
2024-09-05 15:05:40 +0300

    Added changes

--- lib/views/details_page.dart
+++ lib/views/details_page.dart
@@ -1,10 +1,49 @@
 import 'package:flutter/material.dart';
 
+import '../constants/constants.dart';
+
 class DetailsPage extends StatelessWidget {
   const DetailsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Constants.primary,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    height: 40,
+                    width: 40,
+                    decoration: const BoxDecoration(
+                        shape: BoxShape.circle, color: Constants.greenBg),
+                    child: IconButton(
+                      onPressed: () => {},
+                      icon: const Icon(Icons.dashboard),
+                    ),
+                  ),
+                  Container(
+                    height: 40,
+                    width: 40,
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                        image: NetworkImage(Constants.profileImage),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
